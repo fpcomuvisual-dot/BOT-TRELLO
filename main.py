93ba0927e8e141bdf6abec2516dfdbaf58272949
fpcomuvisual-dot <fpcomuvisual@gmail.com>
2025-12-02 13:14:33 -0300

    Adicionando Dockerfile para Hugging Face

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ async def receive_trello_event(request: Request):
     return {"status": "received"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=7860, reload=True)
